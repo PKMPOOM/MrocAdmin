acc1250ea362dfdc1f12f018f65c423ab86da669
PKMPOOM <loikpoom@gmail.com>
2024-06-24 19:45:45 +0700

    `feat: Add types and schemas for survey creation form`

---
+++ src/Pages/Admin/Quantitative/Survey/SmartCreate/type.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { StatusOption } from "~/interface/SurveyEditorInterface";
+
+export const GenQuestionListSchema = z.object({
+  label: z.string(),
+  answer: z.array(z.string()),
+  type: z.string(),
+});
+
+export type GenQuestionList = z.infer<typeof GenQuestionListSchema>;
+
+export const CreateSurveySubmitFormSchema = z.object({
+  name: z.string(),
+  status: z.enum(["active", "draft"]),
+  questionData: z.array(GenQuestionListSchema),
+});
+
+export type TSubmitSchema = z.infer<typeof CreateSurveySubmitFormSchema>;
+
+export const SurveyStatusSelectOptions: StatusOption[] = [
+  {
+    value: "Active",
+    label: "Active",
+  },
+  {
+    value: "Draft",
+    label: "Draft",
+  },
+  {
+    value: "Closed",
+    label: "Closed",
+  },
+];
+
+export type SubmitPayload = {
+  name: string;
+  status: string;
+  questionData: GenQuestionList[];
+};
