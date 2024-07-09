2a8e0ec3116ad1be0b57c88b4178f181e242c483
PKMPOOM <loikpoom@gmail.com>
2024-07-09 15:16:07 +0700

    `refactor: Improve error handling in getTextFromBlock`

--- src/Hooks/Utils/getTextFromBlock.ts
+++ src/Hooks/Utils/getTextFromBlock.ts
@@ -1,18 +1,24 @@
 export const getTextFromBlock = (label: string) => {
-  let questionLabel: string;
-  const regex = /"text"\s*:\s*"([^"]+)"/g;
-  const matches = [];
-  let match;
+  try {
+    let questionLabel: string;
+    const regex = /"text"\s*:\s*"([^"]+)"/g;
+    const matches = [];
+    let match;
 
-  while ((match = regex.exec(label)) !== null) {
-    matches.push(match[1]);
+    while ((match = regex.exec(label)) !== null) {
+      matches.push(match[1]);
+    }
+
+    if (matches.length === 0) {
+      questionLabel = label;
+    } else {
+      questionLabel = matches.join(" ").replace(/  /g, " ");
+    }
+
+    return questionLabel;
+  } catch (error) {
+    console.log(error);
+
+    return label;
   }
-
-  if (matches.length === 0) {
-    questionLabel = label;
-  } else {
-    questionLabel = matches.join(" ").replace(/  /g, " ");
-  }
-
-  return questionLabel;
 };
